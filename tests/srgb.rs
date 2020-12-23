5057fc16defc8123aa0ba14fc43e4c532d1ee8a9
LeonineKing1199 <christian.mazakas@gmail.com>
2020-12-23 13:12:30 -0800

    Lint srgb tests + update grayscale test

--- tests/srgb.rs
+++ tests/srgb.rs
@@ -1,20 +1,22 @@
+#![warn(clippy::pedantic)]
+
 extern crate cvr;
 
 use cvr::rgb::iter::{LinearGray, LinearSRGB, SRGBLinear};
 
 #[test]
 fn srgb_to_linear_to_srgb() {
-    let r = [1u8, 2, 3];
-    let g = [4u8, 5, 6];
-    let b = [7u8, 8, 9];
+    let r = [1_u8, 2, 3];
+    let g = [4_u8, 5, 6];
+    let b = [7_u8, 8, 9];
 
-    let mut red_linear = [0f32; 3];
-    let mut green_linear = [0f32; 3];
-    let mut blue_linear = [0f32; 3];
+    let mut red_linear = [0_f32; 3];
+    let mut green_linear = [0_f32; 3];
+    let mut blue_linear = [0_f32; 3];
 
-    let mut red_srgb = [0u8; 3];
-    let mut green_srgb = [0u8; 3];
-    let mut blue_srgb = [0u8; 3];
+    let mut red_srgb = [0_u8; 3];
+    let mut green_srgb = [0_u8; 3];
+    let mut blue_srgb = [0_u8; 3];
 
     cvr::rgb::Iter::new(&r, &g, &b)
         .srgb_to_linear()
@@ -34,9 +36,12 @@ fn srgb_to_linear_to_srgb() {
             blue_srgb[idx] = b;
         });
 
-    assert_eq!(red_linear, [0.000303527, 0.000607054, 0.00091058103]);
-    assert_eq!(green_linear, [0.001214108, 0.001517635, 0.0018211621]);
-    assert_eq!(blue_linear, [0.002124689, 0.002428216, 0.002731743]);
+    assert_eq!(red_linear, [0.000_303_527, 0.000_607_054, 0.000_910_581_03]);
+    assert_eq!(
+        green_linear,
+        [0.001_214_108, 0.001_517_635, 0.001_821_162_1]
+    );
+    assert_eq!(blue_linear, [0.002_124_689, 0.002_428_216, 0.002_731_743]);
 
     assert_eq!(red_srgb, r);
     assert_eq!(green_srgb, g);
@@ -45,14 +50,22 @@ fn srgb_to_linear_to_srgb() {
 
 #[test]
 fn srgb_to_gray() {
-    let r = [1u8, 2, 3];
-    let g = [4u8, 5, 6];
-    let b = [7u8, 8, 9];
+    let r = [1_u8, 2, 3];
+    let g = [4_u8, 5, 6];
+    let b = [7_u8, 8, 9];
 
     let gray: Vec<f32> = cvr::rgb::Iter::new(&r, &g, &b)
         .srgb_to_linear()
         .linear_to_gray()
         .collect();
 
-    assert_eq!(gray, [0.00108622, 0.001389747, 0.0016932739]);
+    assert_eq!(gray, [0.001_086_22, 0.001_389_747, 0.001_693_273_9]);
+
+    let gray: Vec<u8> = cvr::rgb::Iter::new(&r, &g, &b)
+        .srgb_to_linear()
+        .linear_to_gray()
+        .map(cvr::rgb::linear_to_srgb)
+        .collect();
+
+    assert_eq!(gray, [4, 5, 6]);
 }
